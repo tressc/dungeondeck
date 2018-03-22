85f5d3d1402669f224e907e8bef87b4455476304
Colin <tresscd@gmail.com>
2018-03-22 11:17:46 -0400

    Display deck count on draw

--- bundle.js
+++ bundle.js
@@ -410,6 +410,7 @@ class View {
     this.$root = $root;
 
     this.setupBoard();
+    this.bindEvents();
   }
 
   setupBoard() {
@@ -417,6 +418,13 @@ class View {
     $div.addClass("deck");
     $div.text(this.board.Deck.count);
     this.$root.append($div);
+  }
+
+  bindEvents() {
+    this.$root.on("click", "div", (event => {
+      this.board.Deck.draw(3);
+      $(event.currentTarget).text(this.board.Deck.count);
+    }));
   }
 }
 
