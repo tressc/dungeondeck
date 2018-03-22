ae6760c9eef95b4a176596672a7ff1f8dae218f9
Colin <tresscd@gmail.com>
2018-03-22 12:04:09 -0400

    Burn card on click

--- bundle.js
+++ bundle.js
@@ -414,16 +414,42 @@ class View {
   }
 
   setupBoard() {
-    const $div = $("<div>");
-    $div.addClass("deck");
-    $div.text(this.board.Deck.count);
-    this.$root.append($div);
+    const $deck = $("<div>");
+    $deck.addClass("deck");
+    $deck.text(this.board.Deck.count);
+
+    const $row = $("<ul>");
+    for (let rowIdx = 0; rowIdx < 4; rowIdx++) {
+      let $space = $("<li>");
+      $space.data("pos", rowIdx);
+      if (this.board.DungeonRow.spaces[rowIdx].length > 0) {
+        $space.text(this.board.DungeonRow.spaces[rowIdx][0].suit);
+      } else {
+        $space.text("");
+      }
+      $row.append($space);
+    }
+
+
+    const $drow = $("<div>");
+    $drow.addClass("drow");
+    $drow.append($row);
+
+
+    this.$root.append($deck);
+    this.$root.append($drow);
   }
 
   bindEvents() {
-    this.$root.on("click", "div", (event => {
-      this.board.Deck.draw(3);
+    this.$root.on("click", ".deck", (event => {
+      console.log(this.board.Deck.draw(3));
       $(event.currentTarget).text(this.board.Deck.count);
+    }));
+
+    this.$root.on("click", "li", (event => {
+      const pos = $(event.currentTarget).data("pos");
+      const card = this.board.DungeonRow.spaces[pos][0];
+      this.board.burnCard(card);
     }));
   }
 }
